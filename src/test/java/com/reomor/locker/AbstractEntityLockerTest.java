a4d4ebfb7e0bbe4b7bda38494d9d8d3201316543
Reomor <reomor@gmail.com>
2022-02-28 16:44:07 +0300

    task-11 tests refactoring

---
+++ src/test/java/com/reomor/locker/AbstractEntityLockerTest.java
@@ -0,0 +1,9 @@
+package com.reomor.locker;
+
+public abstract class AbstractEntityLockerTest {
+
+  static final String TEST_ID = "TEST_ID";
+  static final String TEST_ID2 = "TEST_ID2";
+  static final String TEST_ID3 = "TEST_ID3";
+  static final Class<?> TEST_ENTITY_CLASS = Object.class;
+}
